c41a4180b45610d768b138770d14583f32ce0afe
Chrisphine10 <ondiekchrisphine@gmail.com>
2020-12-11 22:39:23 +0300

    alll

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -12,11 +12,10 @@ Rails.application.routes.draw do
     get :who_bought, on: :member
   end
 
-  scope '(:locale)' do
-    resources :orders
-    resources :line_items
-    resources :carts
-    root 'store#index', as: 'store_index', via: :all
-  end
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index', via: :all
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

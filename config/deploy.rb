5687741ca9e47387be4464562d4b30cca8e3a8e4
Chrisphine10 <ondiekchrisphine@gmail.com>
2021-01-14 12:14:21 +0000

    remove

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,3 +60,5 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+
+

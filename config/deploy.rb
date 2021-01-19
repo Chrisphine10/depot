242177d2f8d0ff197552bfad13ece6a5fca1ada2
Chrisphine10 <ondeikchrisphine@gmail.com>
2021-01-19 16:50:02 +0000

    aklalala

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,13 +57,12 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
-set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.6.5'
+#set :rvm_type, :user
+#set :rvm_ruby_version, 'ruby-2.6.5'
 set :linked_files, %w{config/database.yml}
 
 
